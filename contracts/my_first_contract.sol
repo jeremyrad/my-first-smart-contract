0ffe007135e35babe77635f44443c12e5d32f288
"Jeremy" <"jeremyrad@gmail.com">
2022-06-25 16:35:21 +0200

    Examples of data types

--- contracts/my_first_contract.sol
+++ contracts/my_first_contract.sol
@@ -3,16 +3,13 @@ pragma solidity ^0.8.9;
 
 contract MyFirstContract {
 
-    string value;
-
-    constructor() {
-        value = "HelloWorld";
-    }
-
-    function get() public view returns(string memory) {
-        return value;
-    }
-
+    string public value = "Hello World";
+    bool public myBool = true;
+    int public myInt = 1;
+    uint public myUInt = 1;
+    uint8 public myUInt8 = 8;
+    uint256 public myUInt256 = 99999;
+    
     function set(string memory _value) public {
         value = _value;
     }
