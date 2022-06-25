70fa694bb1532622a23f9902407444b9c5e80407
"Jeremy" <"jeremyrad@gmail.com">
2022-06-25 16:43:30 +0200

    Examples using enum

--- contracts/my_first_contract.sol
+++ contracts/my_first_contract.sol
@@ -9,7 +9,24 @@ contract MyFirstContract {
     uint public myUInt = 1;
     uint8 public myUInt8 = 8;
     uint256 public myUInt256 = 99999;
-    
+
+    enum State { Waiting, Ready, Active }
+
+    State public state;
+
+    constructor() {
+        state = State.Waiting;
+    }
+
+    function activate() public {
+        state = State.Active;
+    }
+
+    function isActive() public view returns(bool) {
+        return state == State.Active;
+    }
+
+
     function set(string memory _value) public {
         value = _value;
     }
